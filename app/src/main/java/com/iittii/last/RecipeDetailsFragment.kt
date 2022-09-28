41bbebdf00c769e565b2c65a3d376c1d0ecb1f75
rahmabadar1 <rahmabadar17@gmail.com>
2022-09-28 23:17:05 +0200

    rahma

--- app/src/main/java/com/iittii/last/RecipeDetailsFragment.kt
+++ app/src/main/java/com/iittii/last/RecipeDetailsFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.iittii.last.databinding.FragmentDetailsBinding
 import com.iittii.last.model.Result
+import com.iittii.last.util.Constants
+import com.squareup.picasso.Picasso
 
 class RecipeDetailsFragment : Fragment() {
     private lateinit var binding: FragmentDetailsBinding
@@ -22,7 +24,14 @@ class RecipeDetailsFragment : Fragment() {
 
     private fun details() {
         arguments?.let {
-        val detail= it.getString("result","")
+        val detail= it.get("result") as Result
+            Picasso.get().load(detail.image).into(binding.mainImageView)
+            binding.titleTv.text=detail.title
+            binding.likesTextView.text= detail.aggregateLikes.toString()
+            binding.clockTextView.text=detail.readyInMinutes.toString()
+
+
+
         }
     }
 
